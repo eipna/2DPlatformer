582d9a4a63833792571278964e4561db73b765ea
eipna <Xirveipna@tuta.io>
2024-05-14 00:28:26 +0800

    Added particle for coin

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -3,5 +3,6 @@ extends Area2D
 @onready var game_manager = %GameManager
 
 func _on_body_entered(body):
+	$CPUParticles2D.emitting = true
 	game_manager.add_point()
 	$AnimationPlayer.play("pickup")
