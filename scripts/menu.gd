20aa86b98f27682b67b46a1791a5df153cda512a
eipna <Xirveipna@tuta.io>
2024-05-14 22:27:39 +0800

    Added main menu

---
+++ scripts/menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://.godot/exported/133200997/export-446b1ed11cc7129b20b35ff4df2b3752-level_1.scn")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
