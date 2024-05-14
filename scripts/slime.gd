df0dae9898ea944d9e711b1a8f1d7edc7f4db63f
eipna <Xirveipna@tuta.io>
2024-05-14 16:18:36 +0800

    Added particles for slime

--- scripts/slime.gd
+++ scripts/slime.gd
@@ -5,6 +5,7 @@ var direction: int = 1
 var slime_points: int = 0
 
 @onready var game_manager = %GameManager
+@export var particle = CPUParticles2D
 
 func _process(delta):
 	
@@ -18,5 +19,6 @@ func _process(delta):
 	position.x += direction * SPEED * delta
 
 func _on_explode_trigger_body_entered(body):
+	$CPUParticles2D.emitting = true
 	game_manager.add_slime_point()
 	$AnimationPlayer.play("explode")
